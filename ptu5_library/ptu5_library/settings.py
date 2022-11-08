4ee45daf9aa72b18efd645e6e20a07b3b2b4f360
Sergejus Bykovas <sergejus83@gmail.com>
2022-11-08 10:30:45 +0200

    django 3

--- ptu5_library/ptu5_library/settings.py
+++ ptu5_library/ptu5_library/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from . import local_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ngbyhk!$w@e#(iy2_w^((%^_#zk&w+z4)rtn!)27k82qil2cf6'
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

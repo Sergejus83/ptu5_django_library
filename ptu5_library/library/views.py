376e51c6c882dac5ef2d69f084e9c9640992e595
Sergejus Bykovas <sergejus83@gmail.com>
2022-11-09 15:53:30 +0200

    sablonai - html

--- ptu5_library/library/views.py
+++ ptu5_library/library/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Genre, Author, Book, BookInstance
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Sveiki atvyke!")
+    # return HttpResponse("Sveiki atvyke!")
+    book_count = Book.objects.count()
+    book_intance_count = BookInstance.objects.count()
+    book_instance_availible_count = BookInstance.objects.filter(status='a').count()
+    author_count = Author.objects.count()
+    genre_count = Genre.objects.count()
+
+    context = {
+        'book_count': book_count,
+        'book_intance_count': book_intance_count,
+        'book_intance_availible': book_instance_availible_count,
+        'author_count': author_count,
+        'genre_count': genre_count,
+    }
+
+    return render(request, 'library/index.html', context)
 
     
